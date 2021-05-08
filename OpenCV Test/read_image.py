4390ac016aeadd246afe5a917cbff7ad3ea4e1ca
erickveg <eric.kjvp1@hotmail.com>
2021-05-08 13:40:57 -0600

    updated comments

--- OpenCV Test/read_image.py
+++ OpenCV Test/read_image.py
@@ -1,16 +1,12 @@
 import cv2 as cv 
 import sys
 
-img = cv.imread("Gtav_auto.jpg", cv.IMREAD_UNCHANGED)
+# Read the image.
+img = cv.imread("OpenCV Test/Gtav_auto.jpg", cv.IMREAD_UNCHANGED)
 
+# Message if the image is not found.
 if img is None:
     sys.exit("Could not read the image")
-
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
-
-if k == ord("s"):
-    cv.imwrite("gtav_auto.png", img)
 
 scale_percent = 40 # percent of original size
 width = int(img.shape[1]*scale_percent/100)
@@ -21,5 +17,12 @@ dim = (width, height)
 resized = cv.resize(img, dim, interpolation = cv.INTER_AREA)
 
 cv.imshow("Resize image", resized)
-cv.waitKey(0)
+
+# The time image will be displayed. "0": for undefined time
+k = cv.waitKey(0)
+
+# If the user press "s", a new resized image will be saved
+if k == ord("s"):
+    cv.imwrite("OpenCV Test/gtav_auto.png", img)
+
 cv.destroyAllWindows()
--- OpenCV Test/read_webcam.py
+++ OpenCV Test/read_webcam.py
@@ -1,5 +1,7 @@
 import cv2 as cv 
 
+# Choose a video file to read or provide and integer. "0" for webcam, 
+# "1" for the next cam, "2" for the next one and so on.
 capture = cv.VideoCapture(0)
 
 while True:
